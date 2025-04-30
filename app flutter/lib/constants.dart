1b662024450f4cd6053ccfb4687b4613a6fe494a
Eduardo Bolognini <76057233+eduardo-bolognini@users.noreply.github.com>
2025-04-30 09:11:42 +0200

    constants.dart

--- app flutter/lib/constants.dart
+++ app flutter/lib/constants.dart
@@ -1,11 +1,11 @@
 class Constants {
   // URL base per le API (con endpoint /api)
-  static const String url = "192.168.68.146";
+  static const String url = "";
   static const String apiBaseUrl = 'http://$url:8880/api';
   // URL per la connessione Socket (ws://...)
   static const String socketUrl = 'ws://$url:8880';
 
-  static const String apikey = "HZ2A5B1fbK2RBvq7D8ljDQluCnWIX+npnmOHGqucMGZj5DYUeLWMGA==";
-  static const String wakeWordPath = "assets/porcupine/O-Domina_it_android_v3_0_0.ppn";
-  static const String modelpath = "assets/porcupine/Porcupine Params IT.pv";
+  static const String apikey = "";
+  static const String wakeWordPath = "";
+  static const String modelpath = "";
 }
